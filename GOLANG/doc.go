a4118d26b17d410c30021604021a8f3f9f73c005
Carlo Seghi <carlo.seghi@gmail.com>
2024-10-11 23:06:47 +0200

    Update doc.go

--- GOLANG/doc.go
+++ GOLANG/doc.go
@@ -1,1 +1,6 @@
+// Copyright 2024 Carlo Seghi github.com/acs48. All rights reserved.
+// Use of this source code is governed by a GNU General Public License v3.0
+// license that can be found in the LICENSE file.
 
+// package rpcmple description
+package rpcmple
